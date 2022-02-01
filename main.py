18df6837e30152396a29642530e91e58859f96e1
Shivang <shivangsudhirsonker@gmail.com>
2022-02-01 15:09:03 +0530

    added refresh after conda install

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ install_commands = {
     "conda or miniconda": [
         "", "curl -sL \
       \"https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh\" >\
-      \"Miniconda3.sh\" && bash Miniconda3.sh -b && conda init"
+      \"Miniconda3.sh\" && bash Miniconda3.sh -b && . ~/.bashrc && conda init"
     ]
 }
 
